0989cb7ccf630edd3a0ee1400f9fb381ac0d2c03
ukwally <ukwally22@gmail.com>
2022-05-29 17:44:11 +0100

    a criar o shopping

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="cabcima">
+        <h1>Shopping</h1>
+        <div>
+        <button> LogIn</button>
+        <button> Increver-se</button>
+        </div>
+      </div>
+
+      <div className="cabmeio">
+        <p>Explorar</p>
+        <p>Carreiras</p>
+      </div>
+
+      <div className="cabbaixo">
+        <div className="pesquisa"><input type="text" placeholder="Pesquisar..."></input></div>
+
+        <div className=" servicos">
+          <button> Informática 
+          <ul>
+            <li>Computador</li>
+            <li>Disco</li>
+            <li>Telemóvel</li>
+            <li>Auricular</li>
+            <li>Carregador</li>
+            <li>Mause</li>
+          </ul>
+          </button>
+          <button> Restauração
+          <ul>
+            <li>Bebidas</li>
+            <li>Produtos pasteleiros</li>
+            <li>Cusinhados</li>
+          </ul>
+          </button>
+          <button> Prestação de Serviços
+          <ul>
+            <li>Técnico de Frio</li>
+            <li>Técnico Rádio</li>
+            <li>Pintura</li>
+            <li>Arquitectura</li>
+            <li>Electricidade</li>
+            <li>Mecânica auto</li>
+          </ul>
+
+          </button>
+
+        </div>
+           
+      </div>
+      
+
       </header>
+
+      <div>
+
+      </div>
     </div>
+    
   );
 }
 
